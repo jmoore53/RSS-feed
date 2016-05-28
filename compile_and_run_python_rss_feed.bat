7f5ac9b187a064002ed360b58973e0472eb60117
jmoore53 <jmoore6786wjm@hotmail.com>
2016-05-28 10:21:19 -0400

    Batch file will open .html file

--- compile_and_run_python_rss_feed.bat
+++ compile_and_run_python_rss_feed.bat
@@ -1,2 +1,3 @@
 echo "This is the batch file to get the most recent news from rss feeds."
 python "C:\Users\William\Documents\Programming\Python\RSS-feed\Python_RSS_get.py"
+start C:\Users\William\Documents\Programming\Python\RSS-feed\index.html
