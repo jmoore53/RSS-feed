fbc3204c725b9b34f389fcf3bb120d74827cd92b
jmoore53 <jmoore6786wjm@hotmail.com>
2016-06-15 17:38:30 -0400

    .bat file needs to be user specific

--- compile_and_run_python_rss_feed.bat
+++ compile_and_run_python_rss_feed.bat
@@ -1,3 +1,5 @@
+rem This file only works for my project folders
+	
 echo "This is the batch file to get the most recent news from rss feeds."
 python "C:\Users\William\Documents\Programming\Python\RSS-feed\Python_RSS_get.py"
 start C:\Users\William\Documents\Programming\Python\RSS-feed\index.html
